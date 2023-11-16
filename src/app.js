6d4b9992759dd04325ee9b1b344fbf065344c440
Giuseppe <132507608+GiuseppeTaylorr@users.noreply.github.com>
2023-11-16 23:28:09 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -3,45 +3,54 @@ import "bootstrap";
 import "./style.css";
 import "./assets/img/4geeks.ico";
 
-function generateNumber() {
+let numberSup = document.getElementById("numero-superior");
+let numberInf = document.getElementById("numero-inferior");
+let simbolo = document.getElementById("simbolo");
+function generateNumber(numberDom) {
   let randomNumber = Math.floor(Math.random() * 12) + 1;
   let isEven = (Math.floor(Math.random() * 1000) + 100) % 2 == 0;
-  let numberDom = document.getElementById("numero");
-  let topSpan = numberDom.querySelector(".top");
-  let bottomSpan = numberDom.querySelector(".bottom");
   let cardSymbol = generateCardSymbol();
-  //Establecer el texto interno
-  numberDom.innerHTML = `${randomNumber} ${cardSymbol}`;
-  //Agregar una clase de estilo
-  numberDom.classList.add(isEven ? "rojo" : "negro");
-  console.log({ isEven });
+  numberDom.forEach(element => {
+    //Establecer el texto interno
+    if (element == simbolo) {
+      element.innerHTML = ` ${cardSymbol}`;
+    } else {
+      element.innerHTML = `${randomNumber} ${cardSymbol}`;
+    }
+
+    //Agregar una clase de estilo
+    element.classList.add(isEven ? "rojo" : "negro");
+    console.log({ isEven });
+  });
 }
 function generateCardSymbol() {
   const colores = ["♣", "♥", "♦", "♠"];
   const colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
   return `${colorAleatorio}`;
 }
-function changeColor(e) {
-  let numberDom = document.querySelector("#numero");
-  if (numberDom.classList.contains("negro")) {
-    numberDom.classList.remove("negro");
-    numberDom.classList.add("rojo");
-  } else {
-    numberDom.classList.remove("rojo");
-    numberDom.classList.add("negro");
-  }
+function changeColor(numberDom) {
+  numberDom.forEach(element => {
+    if (element.classList.contains("negro")) {
+      element.classList.remove("negro");
+      element.classList.add("rojo");
+    } else {
+      element.classList.remove("rojo");
+      element.classList.add("negro");
+    }
+  });
 }
 window.onload = function() {
   //write your code here
-  generateNumber();
+  generateNumber([numberSup, numberInf, simbolo]);
   document
     .getElementsByClassName("card")
     .item(0)
-    .addEventListener("click", generateNumber);
+    .addEventListener("click", generateNumber([numberSup, numberInf, simbolo]));
   let button = document.createElement("button");
   //button.innerText = cambiar de color
   (button.innerHTML = "<i>Cambia color</i>"),
-    (button.addEventListener = ("click", changeColor));
+    (button.addEventListener =
+      ("click", changeColor([numberSup, numberInf, simbolo])));
   document.querySelector(".container");
   //.insertAdjacentElement("afterBegin", button);
 };
